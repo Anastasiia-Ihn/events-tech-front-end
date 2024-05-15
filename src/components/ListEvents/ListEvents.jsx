4cceb7ad6f715821b0ef442ce53d9ca7ff2d1b3e
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-05-15 19:19:08 +0200

    add pagination

--- src/components/ListEvents/ListEvents.jsx
+++ src/components/ListEvents/ListEvents.jsx
@@ -1,29 +1,57 @@
 import css from "./ListEvents.module.css";
 
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import CardEvent from "../CardEvent/CardEvent";
 import { getAllEvents } from "../../getData/getData";
 
 const ListEvents = () => {
   const [events, setEvents] = useState([]);
+  const [isMoreEvents, setIsMoreEvents] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
+    const currentPage = searchParams.get("page") ?? "";
+    console.log(currentPage);
+    if (currentPage === "") {
+      setSearchParams({ page: 1 });
+      return;
+    }
+
     async function fetchData() {
       try {
-        const eventsAll = await getAllEvents();
+        const eventsAll = await getAllEvents(currentPage);
+
+        if (!eventsAll.length) {
+          setIsMoreEvents(false);
+          throw Error("Sorry");
+        }
+
         setEvents((prev) => [...prev, ...eventsAll]);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
-  }, []);
+  }, [searchParams, setSearchParams]);
+
+  const handleNextPage = () => {
+    const currentPage = searchParams.get("page");
+    const nextPage = Number(currentPage) + 1;
+
+    setSearchParams({ page: nextPage });
+  };
+
   return (
-    <ul className={css.list}>
-      {events?.map((el) => (
-        <CardEvent key={Date.now()} event={el} />
-      ))}
-    </ul>
+    <>
+      <ul className={css.list}>
+        {events?.map((el) => (
+          <CardEvent key={Date.now() * Math.random()} event={el} />
+        ))}
+      </ul>
+      {isMoreEvents && <button onClick={handleNextPage}>LoadMore</button>}
+    </>
   );
 };
 
